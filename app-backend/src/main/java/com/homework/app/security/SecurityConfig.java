1c4dcd437b0a3fe714fff1dc933551df6cdae64a
Bartholomeusz <shehan.bartholomeusz@pearson.com>
2021-12-20 09:52:22 +0530

    Authentication and authorization implemented

---
+++ app-backend/src/main/java/com/homework/app/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.homework.app.security;
+
+import com.homework.app.filter.CustomAuthenticationFilter;
+import com.homework.app.filter.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.homework.app.util.Util.*;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+
+        http.authorizeRequests().antMatchers(GET, "api/login/**", "api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "api/homework/**").hasAnyAuthority(VIEWER);
+        http.authorizeRequests().antMatchers(POST, "api/homework/**").hasAnyAuthority(ADMIN, EDITOR);
+        http.authorizeRequests().antMatchers(PUT, "api/homework/**").hasAnyAuthority(ADMIN, EDITOR);
+        http.authorizeRequests().antMatchers(DELETE, "api/homework/**").hasAnyAuthority(ADMIN, EDITOR);
+        http.authorizeRequests().anyRequest().authenticated();
+
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
